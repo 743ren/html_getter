9d0fee0f9cc1e6ecff00d0be6f915b3023ecde2b
mazhijun <743ren@gmail.com>
2024-08-26 16:23:59 +0800

    加个超时

--- lib/request.dart
+++ lib/request.dart
@@ -1,20 +1,28 @@
+import 'dart:async';
+
 import 'package:desktop_webview_window/desktop_webview_window.dart';
 
 class Request {
   Webview? webview;
   Function()? listener;
+  Timer? timer;
 
-  void request(String url, Function(String?) callback) async {
+  void request(String url, Function(String?) callback, {Duration timeout = const Duration(seconds: 10)}) async {
     webview ??= await WebviewWindow.create(
       configuration: const CreateConfiguration(
         windowHeight: 0,
         windowWidth: 0,
       ),
     );
+    timer = Timer(timeout, () {
+      callback(null);
+    });
     if (listener == null) {
       // 没有用 Widget，就不用 ValueListenableBuilder 了，直接自己 listen
       listener = () async {
-        if (!webview!.isNavigating.value) { // fasle 表示加载据说
+        if (!webview!.isNavigating.value && // fasle 表示加载完成
+            (timer?.isActive ?? false)) { // 如果超时那执行过了，就不用再回调了
+          timer?.cancel();
           var html = await webview!.evaluateJavaScript('document.documentElement.outerHTML');
           // 如果页面加载出错，返回 <html><head></head><body></body></html>
           if (html == r'<html><head></head><body></body></html>') {
@@ -33,6 +41,7 @@ class Request {
     if (listener != null) {
       webview?.isNavigating.removeListener(listener!);
     }
+    timer?.cancel();
   }
 }
 
