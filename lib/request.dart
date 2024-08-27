31d85b3406e7e3aaf2121b399169e93dcaf807cb
mazhijun <743ren@gmail.com>
2024-08-27 15:46:48 +0800

    将回调改成Future

--- lib/request.dart
+++ lib/request.dart
@@ -4,44 +4,44 @@ import 'package:desktop_webview_window/desktop_webview_window.dart';
 
 class Request {
   Webview? webview;
-  Function()? listener;
   Timer? timer;
 
-  void request(String url, Function(String?) callback, {Duration timeout = const Duration(seconds: 10)}) async {
+  Future<String?> request(String url, {Duration timeout = const Duration(seconds: 10)}) async {
     webview ??= await WebviewWindow.create(
       configuration: const CreateConfiguration(
         windowHeight: 0,
         windowWidth: 0,
       ),
     );
+
+    Completer<String?> completer = Completer<String?>();
+
     timer = Timer(timeout, () {
-      callback(null);
+      if (!completer.isCompleted) {
+        completer.complete(null);
+      }
     });
-    if (listener == null) {
-      // 没有用 Widget，就不用 ValueListenableBuilder 了，直接自己 listen
-      listener = () async {
-        if (!webview!.isNavigating.value && // fasle 表示加载完成
-            (timer?.isActive ?? false)) { // 如果超时那执行过了，就不用再回调了
-          timer?.cancel();
-          var html = await webview!.evaluateJavaScript('document.documentElement.outerHTML');
-          // 如果页面加载出错，返回 <html><head></head><body></body></html>
-          if (html == r'<html><head></head><body></body></html>') {
-            html = null;
-          }
-          callback(html);
-          webview!.close();
+
+    webview!.isNavigating.addListener(() async {
+      if (!webview!.isNavigating.value && !completer.isCompleted) {
+        timer?.cancel();
+        var html = await webview!.evaluateJavaScript('document.documentElement.outerHTML');
+        if (html == r'<html><head></head><body></body></html>') {
+          html = null;
         }
-      };
-      webview!.isNavigating.addListener(listener!);
-    }
+        completer.complete(html);
+        webview!.close();
+      }
+    });
+
     webview!.launch(url);
+
+    return completer.future;
   }
 
   void dispose() {
-    if (listener != null) {
-      webview?.isNavigating.removeListener(listener!);
-    }
     timer?.cancel();
+    webview?.close();
   }
 }
 
